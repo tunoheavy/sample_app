a4eebaaa41cf84af2c9d0e19967f188884a270e0
David Verdú <invitado@perdidita.(none)>
2014-04-11 11:48:06 +0100

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -41,6 +41,7 @@ describe "Authentication" do
   describe "authorization" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
       describe "in the Users controller" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -52,6 +53,16 @@ describe "Authentication" do
         end
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_title('Sign in') }
         end
       end
@@ -93,6 +104,19 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(root_url) }
         end
       end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
 
     end
      
